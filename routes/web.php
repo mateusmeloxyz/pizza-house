61483148585b4830ba55520ef0db1df8ba7d093e
mateusgomes01 <mgm6@cin.ufpe.br>
2020-05-29 16:36:03 -0300

    learned views and outputing data throug view

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pizzas', function () {
+    // get data from database
+    $pizza = [
+        'type' => 'hawaiian', 
+        'base' => 'cheesy crust',
+        'price' => 10
+    ];
+    return view('pizzas',$pizza);
+    // can also return text or json
+    // return 'pizzas!';
+    // return ['name' => 'veg pizzas', 'base' => 'classic'];
+});
+
+/**
+ * Route is a class that has a get() method
+ * get takes 2 arguments: 
+ * 1-the rout acording to the root webpage. Ex:
+ * www.mywebapp.com/
+ * 
+ * 2-a function returning a view that is located in the resources/views folder
+ * 
+ */
