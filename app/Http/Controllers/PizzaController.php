8ce1450a26ecc784aa89fdcdf97ba5aa0eff2dab
mateusgomes01 <mgm6@cin.ufpe.br>
2020-06-02 10:44:58 -0300

    Created a form to handle POST request for new pizza

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -27,7 +27,6 @@ class PizzaController extends Controller
     }
 
     public function create() {
-        // use the $id variable to query the db for a record
         return view('pizzas.create');
     }
 }
--- resources/views/pizzas/create.blade.php
+++ resources/views/pizzas/create.blade.php
@@ -1,14 +1,26 @@
 @extends('layouts.layout')
 
 @section('content')
-<div class="flex-center position-ref full-height">
-    
-
-    <div class="content">
-        <div class="title m-b-md">
-            Add a New Pizza
-        </div>
-
-    </div>
+<div class="wrapper create-pizza">
+    <h1>Create a New Pizza</h1>
+    <form action="/pizzas" method="POST">
+        <label for="name">Your name:</label>
+        <input type="text" name="name" id="name">
+        <label for="type">Coose pizza type:</label>
+        <select name="type" id="type">
+            <option value="margherita">Margherita</option>
+            <option value="hawaiian">hawaiian</option>
+            <option value="veg supreme">veg supreme</option>
+            <option value="volcano">volcano</option>
+        </select>
+        <label for="base">Coose base type:</label>
+        <select name="base" id="base">
+            <option value="cheesy crust">Cheesy Crust</option>
+            <option value="garlic crust">Garlic Crust</option>
+            <option value="thin & crispy">Thin & Crispy</option>
+            <option value="thick">Thick</option>
+        </select>
+        <input type="submit" value="Order Pizza">
+    </form>
 </div>
 @endsection
