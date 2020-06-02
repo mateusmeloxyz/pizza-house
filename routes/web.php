25e3e7afe71aa33a553d6e89e045e63fab50c5fb
mateusgomes01 <mgm6@cin.ufpe.br>
2020-06-02 11:01:29 -0300

    Created the POST request controller & route

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,11 @@ Route::get('/pizzas', 'PizzaController@index');
 
 Route::get('/pizzas/create', 'PizzaController@create'); // must come before id route so that create isn't treated as an id
 
+Route::POST('/pizzas', 'PizzaController@store');
+
 Route::get('/pizzas/{id}', 'PizzaController@show');
+
+
 
 
 /**
